8c148f3fbaeda874609d99e359ed140cbd65f0b4
Aļģis Laureckis <algis.laureckis@gmail.com>
2018-04-30 20:06:09 +0300

    Fixed #9.

--- sound.android.js
+++ sound.android.js
@@ -1,28 +1,29 @@
 var common = require("./sound-common");
 
-var Sound = (function (_super) {
+var Sound = (function(_super) {
     __extends(Sound, _super);
+
     function Sound() {
         _super.apply(this, arguments);
 
         this._volume = 1.0;
         this._player = new android.media.SoundPool(1, android.media.AudioManager.STREAM_MUSIC, 0);
         this._soundId = this._player.load(this._path, 1);
+        this._streamId = null;
     }
-    Sound.prototype.setVolume = function (volume) {
-      if(typeof volume === 'number' && volume >= 0.0 && volume <= 1.0)
-        this._volume = volume;
-      else
-        console.error("Volume not set; volume outside of range 0.0 - 1.0");
+    Sound.prototype.setVolume = function(volume) {
+        if (typeof volume === 'number' && volume >= 0.0 && volume <= 1.0)
+            this._volume = volume;
+        else
+            console.error("Volume not set; volume outside of range 0.0 - 1.0");
     };
-    Sound.prototype.play = function () {
-        this._player.play(this._soundId, this._volume, this._volume, 1, 0, 1.0);
+    Sound.prototype.play = function() {
+        this._streamId = this._player.play(this._soundId, this._volume, this._volume, 1, 0, 1.0);
     };
-    Sound.prototype.stop = function () {
-        this._player.stop(this._soundId);
+    Sound.prototype.stop = function() {
+        this._player.stop(this._streamId);
     };
-    Sound.prototype.reset = function () {
-    };
+    Sound.prototype.reset = function() {};
     return Sound;
 })(common.Sound);
 exports.Sound = Sound;
